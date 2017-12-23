de5f18490cefce276beab8b6a6090ed404c1fc82
tkhr-sait <tkhr.sait@gmail.com>
2017-12-23 21:10:49 +0900

    refs #6 フィーチャファイルのExamplesに外部ファイルを指定

--- cucumber-feature-converter/run.sh
+++ cucumber-feature-converter/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp ./target/cucumber-feature-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar scenario.test.CucumberFeatureComverter src/test/resources /tmp
+java -jar ./target/cucumber-feature-converter-0.0.1-SNAPSHOT-jar-with-dependencies.jar src/test/resources /tmp
 
